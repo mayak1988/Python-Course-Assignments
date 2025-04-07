87cc056641029904d4d5b9fa62a3df6ad2b442cb
mayak1988 <maria.kovalevsky@gmail.com>
2025-04-07 10:28:06 +0300

    added HW assignments

---
+++ D02/circle.py
@@ -0,0 +1,4 @@
+import numpy as np
+radius = input("input the radius of the circle: ")
+area = round(np.pi*int(radius)**2,2)
+print(f'The area of the circle is: {area}')
---
+++ D02/rectangle.py
@@ -0,0 +1,3 @@
+height = input("input the height: ")
+width = input("input the width: ")
+print(f'The area of the rectangle is {int(height)*int(width)}')

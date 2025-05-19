f3071436605a199e3db3483af230f6aeab3aeddd
Maya Kovalevsky <73478004+mayak1988@users.noreply.github.com>
2025-05-19 10:23:32 +0300

    Update count_words_in_lists.py

--- D05/count_words_in_lists.py
+++ D05/count_words_in_lists.py
@@ -1,1 +1,11 @@
+# Create a script called count_words_in_lists.py that given a list of words (for now embedded in the program itself) will count how many times each word appears.
+# celestial_objects = [
+#     'Moon', 'Gas', 'Asteroid', 'Dwarf', 'Asteroid', 'Moon', 'Asteroid'
+# ]
+# Expected output:
 
+
+# Moon        2
+# Gas         1
+# Asteroid    3
+# Dwarf       1

f790f8c2b1ae82d6ef4fcac0dfb56571b0dce59b
Maya Kovalevsky <73478004+mayak1988@users.noreply.github.com>
2025-05-19 10:24:35 +0300

    Update count_digits_in_lists.py

--- D05/count_digits_in_lists.py
+++ D05/count_digits_in_lists.py
@@ -1,5 +1,15 @@
 # Create a script called count_digits_in_lists.py that given a list of numbers count how many times each digit appears? The output will look like this:
 
-# celestial_objects = [
-#     'Moon', 'Gas', 'Asteroid', 'Dwarf', 'Asteroid', 'Moon', 'Asteroid'
-# ]
+
+# 0  1
+# 1  3
+# 2  3
+# 3  2
+# 4  1
+# 5  2
+# 6  2
+# 7  0
+# 8  1
+# 9  1
+
+# numbers = [1203, 1256, 312456, 98]

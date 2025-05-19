3128e2e7ec9dd3574a0c0b93980a81d37057cb20
Maya Kovalevsky <73478004+mayak1988@users.noreply.github.com>
2025-05-19 10:22:41 +0300

    Update extended_dna_sequencing.py

--- D05/extended_dna_sequencing.py
+++ D05/extended_dna_sequencing.py
@@ -1,1 +1,7 @@
+# Create a file called extended_dna_sequencing.py
+# In this case the original string contains more than on type of foreign elements: e.g. 'ACCGXXTXXYYGTTQRACQQTGGGCXTTGTXX'.
+# Expected output: ['TGGGC', 'ACCG', 'TTGT', 'GTT', 'AC', 'T']
+# Ask for a sequence on the Standard Input (STDIN) like this:
 
+# python extended_dna_sequencing.py
+# Please type in a sequence:
